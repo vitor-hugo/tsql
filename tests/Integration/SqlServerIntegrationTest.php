cf92cd51a71669c9385297f60e42f53d12243d96
vitor-hugo <vitor83dev@gmail.com>
2024-07-15 11:57:15 -0300

    add: SqlServer integration tests

---
+++ tests/Integration/SqlServerIntegrationTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
+use Tests\Integration\Contracts\BaseIntegrationTestCase;
+use Torugo\Sql\Enums\DBEngine;
+use Torugo\Sql\TSql;
+
+#[Group("Integration")]
+#[Group("TSql")]
+#[TestDox("TSql: SqlServer Integration Tests")]
+class SqlServerIntegrationTest extends BaseIntegrationTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        self::$tsql = new TSql(DBEngine::SqlServer);
+        self::$connectionParams = ["localhost", "TestDB", "SA", "SuperStrongPassword!"];
+        self::$tsql->connect(...self::$connectionParams);
+    }
+
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$tsql->close();
+    }
+
+
+    public function testShouldConnectToDatabase()
+    {
+        $tsql = new TSql(DBEngine::SqlServer);
+        $tsql->connect(...self::$connectionParams);
+        $this->assertTrue($tsql->isConnected());
+        $tsql->close();
+    }
+}
