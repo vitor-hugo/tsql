efb2721ca3c07f7fecab30d36f023ada16d15d82
vitor-hugo <vitor83dev@gmail.com>
2024-07-15 09:53:32 -0300

    add: creating integration tests table

--- docker/sqlserver/create-table.sql
+++ docker/sqlserver/create-table.sql
@@ -1,26 +1,28 @@
-USE TestDB;
+USE TestDB
 
-IF (EXISTS (SELECT *
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'TestDB' AND TABLE_NAME = 'TestTable'))
+IF OBJECT_ID(N'[dbo].[TestTable]', 'U') IS NULL
 BEGIN
-    DELETE FROM TestTable;
+    CREATE TABLE TestTable
+    (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        age INT NOT NULL
+    )
+END ELSE BEGIN
+    DELETE FROM TestTable
 END
 
-DECLARE @sql nvarchar(255);
-WHILE EXISTS(select *
-from INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-where constraint_catalog = 'TestDB' AND table_name = 'TestTable')
+IF OBJECT_ID(N'[dbo].[IntegrationTable]', 'U') IS NULL
 BEGIN
-    SELECT @sql = 'ALTER TABLE TestTable DROP CONSTRAINT ' + CONSTRAINT_NAME
-    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-    WHERE constraint_catalog = 'TestDB' AND table_name = 'TestTable'
-    EXEC sp_executesql @sql
+    CREATE TABLE IntegrationTable
+    (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        age INT NOT NULL
+    )
+END ELSE BEGIN
+    DELETE FROM TestTable
 END
 
-CREATE TABLE TestTable
-(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age INT NOT NULL
-)
+DELETE FROM TestTable;
+DELETE FROM IntegrationTable;
